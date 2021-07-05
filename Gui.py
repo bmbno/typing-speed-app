365cb259d3273c71fefbd7d88109c80c8cb34eb4
bmbno <gng10836@gmail.com>
2021-07-04 20:29:47 -0400

    cleaned up code

--- Gui.py
+++ Gui.py
@@ -2,15 +2,16 @@ import tkinter as tk
 
 
 class Gui:
-    def __init__(self, master):
-        self.master = master
-        master.title("Type Speed Tester")
+    def __init__(self, root):
+        self.root = root
+        root.title("Type Speed Tester")
 
         self.text_entry_box_title = tk.Label(
-            master, text="Type here to start").pack()
-        self.text_entry_box = tk.Entry(master).pack()
-        self.reset_button = tk.Button(master, text="Reset",
+            root, text="Type here to start").pack()
+        self.text_entry_box = tk.Entry(root).pack()
+        self.reset_button = tk.Button(root, text="Reset",
                                       command=self.reset).pack(side="bottom")
+
 
     def reset(self):
         return
